05745578c9910b350a1a894c3d26419edec7ec51
Jeh <46088151+JehkTech@users.noreply.github.com>
2023-05-04 08:48:41 +0200

    Add files via upload
    Source code

---
+++ IPExtractor.py
@@ -0,0 +1,18 @@
+import re
+
+# Open the input file
+with open('C:/users/jeh.k/Documents/RandomIP.txt', 'r') as file:
+    # Read the file content
+    content = file.read()
+
+    # Compile the regular expression pattern for IP addresses
+    pattern = re.compile(r'\b(?:\d{1,3}\.){3}\d{1,3}\b')
+
+    # Find all IP addresses in the file
+    ip_addresses = re.findall(pattern, content)
+
+    # Sort the IP addresses in ascending order
+    ip_addresses.sort()
+    # Print the sorted IP addresses
+    for ip_address in ip_addresses:
+        print(ip_address)
